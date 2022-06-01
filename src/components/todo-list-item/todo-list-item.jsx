f9bf03c7de714642782401c5c16a956bec3c126b
NazarovAsadbek <nazarov-asadbek-official@mail.ru>
2022-06-01 16:50:00 +0500

    Course: React. Juriy Buka. Part 3. React components.
    Last lesson: 36

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -3,21 +3,30 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+  state = {
+    done: false
+  }
+
   onLabelClick = () => {
-    console.log(this.props.label)
+    this.setState({
+      done: !this.state.done
+    })
   }
 
   render() {
     const {label, important = false} = this.props;
-
+    const {done} = this.state;
+    let classNames = 'todo-list-item';
+    if (done) classNames += ' done';
     const style = {
       color: important ? 'steelblue' : 'black', fontWeight: important ? 'bold' : 'normal'
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
       <span
-        className="todo-list-item-label"
+        className="todo-list-item-label "
         style={style}
         onClick={this.onLabelClick}
       >
