0b61abec8afc2a9b41d1a128358a3d91beee68df
NazarovAsadbek <nazarov-asadbek-official@mail.ru>
2022-06-02 08:38:12 +0500

    Course: React. Juriy Buka. Part 3. React components. SetState()
    Last lesson: 37

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -5,7 +5,8 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
 
   state = {
-    done: false
+    done: false,
+    important: false
   }
 
   onLabelClick = () => {
@@ -14,27 +15,35 @@ export default class TodoListItem extends Component {
     })
   }
 
+  onMarkImportant = () => {
+    this.setState({
+      important: !this.state.important
+    })
+  }
+
   render() {
-    const {label, important = false} = this.props;
-    const {done} = this.state;
+    const {label} = this.props;
+    const {done, important} = this.state;
     let classNames = 'todo-list-item';
     if (done) classNames += ' done';
-    const style = {
-      color: important ? 'steelblue' : 'black', fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ' important'
+    }
 
     return (
       <span className={classNames}>
       <span
         className="todo-list-item-label "
-        style={style}
         onClick={this.onLabelClick}
       >
         {label}
       </span>
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+        onClick={this.onMarkImportant}
+      >
         <i className="fa fa-exclamation"/>
       </button>
 
