35391895f3480c3098a7e01a2d38ed6cc7b2d044
Асадбек К. Назаров <a.nazarov@crowe.local>
2022-06-02 10:38:27 +0500

    Don' change object || array
    Course: React. Juriy Buka.
    Part 3. React components.
    Don' change object || array
    Last lesson: 40

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './app.css';
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from "../item-status-filter";
 
-const ToDo = () => {
-  const todoData = [
-    {id: 1, label: 'Drink Coffee', important: false},
-    {id: 2, label: 'Make Awesome App', important: true},
-    {id: 3, label: 'Have a lunch', important: false},
-    {id: 4, label: 'Go to bed', important: true},
-  ]
+export default class ToDo extends Component {
+  state = {
+    todoData: [
+      {id: 1, label: 'Drink Coffee', important: false},
+      {id: 2, label: 'Make Awesome App', important: true},
+      {id: 3, label: 'Have a lunch', important: false},
+      {id: 4, label: 'Go to bed', important: true},
+    ]
+  }
 
-  return (
-    <div className="todo-app">
-      <AppHeader/>
-      <div className="d-flex top-panel">
-        <SearchPanel/>
-        <ItemStatusFilter/>
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const index = todoData.findIndex((el) => el.id === id);
+      const newArray = [
+        ...todoData.slice(0, index),
+        ...todoData.slice(index + 1)
+      ]
+
+      return {
+        todoData: newArray
+      }
+    })
+  }
+
+  render() {
+    const {todoData} = this.state;
+
+    return (
+      <div className="todo-app">
+        <AppHeader/>
+        <div className="d-flex top-panel">
+          <SearchPanel/>
+          <ItemStatusFilter/>
+        </div>
+        <TodoList onDeleted={(id) => this.deleteItem(id)} todos={todoData}/>
       </div>
-      <TodoList onDeleted={(id) => console.dirxml(id)} todos={todoData}/>
-    </div>
-  )
+    )
+  }
 };
-
-export default ToDo;
