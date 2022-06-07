f7d0b3fb3c4e2d554f6e7ba13be2c23880470afe
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-06-07 11:15:35 +0500

    Two way binding input
    Course: React. Juriy Buka.
    Part 4. React components.
    Two way binding input

--- src/components/item-add-form/item-add-form.jsx
+++ src/components/item-add-form/item-add-form.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './item-add-form.css';
 
-const ItemAddForm = ({addToDo}) => {
-  return (
-    <div className="item-add-form bottom-panel new-todo-label">
-      <button
-        className="btn btn-outline-secondary"
-        onClick={() => addToDo('hello world')}
-      >
-        Add Item
-      </button>
-    </div>
-  )
-}
+export default class ItemAddForm extends Component {
+  state = {
+    label: ''
+  }
+  onLabelChange = (e) => {
+    this.setState({
+      label: e?.target.value || ''
+    })
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addToDo(this.state.label);
 
-export default ItemAddForm
+    this.onLabelChange()
+  }
+
+  render() {
+    return (
+      <form
+        className="item-add-form bottom-panel new-todo-label d-flex"
+        onSubmit={this.onSubmit}
+      >
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={this.onLabelChange}
+          value={this.state.label}
+        />
+        <button
+          type="submit"
+          className="btn btn-outline-secondary"
+        >
+          Add Item
+        </button>
+      </form>
+    )
+  }
+}
